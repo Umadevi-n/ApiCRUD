35b06481d37e5510e133aaa2cbe8c111cf094e0f
Quadrant <Quadrant@DESKTOP-FEKK5US>
2021-08-26 18:23:28 +0530

    Add project files.

---
+++ ApiCRUD/Data/EmployeeCrudContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiCRUD.Models;
+
+namespace ApiCRUD.Data
+{
+    public class EmployeeCrudContext : DbContext
+    {
+        public EmployeeCrudContext (DbContextOptions<EmployeeCrudContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiCRUD.Models.Employee> Employee { get; set; }
+    }
+}
+
